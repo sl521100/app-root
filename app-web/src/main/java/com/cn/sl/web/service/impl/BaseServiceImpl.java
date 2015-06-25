ff193f20149b64773135d8db90c30b02c54f1897
sl521100 <sl521100@gmail.com>
2015-06-25 14:51:43 +0800

    aa

---
+++ app-web/src/main/java/com/cn/sl/web/service/impl/BaseServiceImpl.java
@@ -0,0 +1,16 @@
+package com.cn.sl.web.service.impl;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.Serializable;
+
+/**
+ * Created by jimmy on 2015/1/15.
+ */
+public abstract class BaseServiceImpl implements Serializable {
+
+    protected Logger logger = LogManager.getLogger();
+
+
+}
